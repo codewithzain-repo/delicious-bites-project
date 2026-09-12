35058ae52392726573446570b3d3204493b32b14
Muhammad Zain <101573208+codewithzain-repo@users.noreply.github.com>
2026-09-12 21:14:09 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+        const hamburger = document.getElementById('hamburger');
+        const navMenu = document.getElementById('nav-menu');
+
+        if (hamburger && navMenu) {
+            hamburger.addEventListener('click', function() {
+                navMenu.classList.toggle('active'); 
+                hamburger.classList.toggle('open'); 
+            });
+        }
+    });
